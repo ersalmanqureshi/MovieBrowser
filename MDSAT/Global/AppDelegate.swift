09d27b4cb55cd89064e33ac8036419c5051a18f3
Salman Qureshi <er.salmanqureshi@gmail.com>
2018-05-25 16:34:33 +0530

    Organise the source file

---
+++ MDSAT/Global/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  MDSAT
+//
+//  Created by Salman Qureshi on 5/25/18.
+//  Copyright © 2018 Salman Qureshi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
