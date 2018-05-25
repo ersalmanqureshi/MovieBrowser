3d50c6a5daa84271540891431bc917b44774784e
Salman Qureshi <er.salmanqureshi@gmail.com>
2018-05-25 16:20:11 +0530

    Initial Commit

---
+++ MDSAT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MDSAT
+//
+//  Created by Salman Qureshi on 5/25/18.
+//  Copyright © 2018 Salman Qureshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
